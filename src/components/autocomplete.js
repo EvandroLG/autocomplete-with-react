df087c2fd0e7e2758b7ad5ab910cc7531465df17
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-02-20 19:29:20 -0300

    save and update autocompleteItems in states in autocomplete component

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import FuzzySearch from 'fuzzy-search';
+import AutocompleteItem from './autocomplete_item';
+import Search from '../lib/search';
+import debounce from '../lib/debounce';
 
 class Autocomplete extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.search = new Search();
+    this._onInputChange = debounce(this._onInputChange, 300);
+  }
+
+  _onInputChange(value) {
+    let stateItems = this.state.items;
+    let size = 0;
+
+    this.search.getItems(value).forEach((item) => {
+      size = stateItems.length;
+      stateItems.push(<AutocompleteItem key={size} value={item} />);
+    });
+
+    this.setState({
+      items: stateItems
+    });
+  }
+
   render() {
     return (
-      <input type="text" id="autocomplete" />
+      <div>
+        <input type="text" id="autocomplete" onChange={ (e) => { this._onInputChange(e.target.value); } } />
+
+        <ul>
+          {
+            this.state.items.map((item) => {
+              return item;
+            })
+          }
+        </ul>
+      </div>
     )
   }
 };
