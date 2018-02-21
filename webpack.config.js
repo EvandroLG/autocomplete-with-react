6ee246a5fe75ca3319518638dfa48898184830d9
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-02-20 23:24:12 -0300

    update webpack build - now I'm generating the build at the root of the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const currentDir = process.cwd();
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: currentDir
   },
   module: {
     loaders: [{
@@ -20,6 +20,6 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: currentDir
   }
 };
